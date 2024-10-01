f9f609f2861beac7f4e4808988b40b08c2080e06
Nguyen-VuHa <havu20111999@gmail.com>
2024-10-01 09:41:01 +0700

    setup project using typescript and tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Thêm các file TS và TSX
+  ],
+  theme: {
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
+
